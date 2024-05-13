970c1c4b944ca43a9064772e3b30afbb04d8ac70
Augusto Castilho Borges <augustocastilhoborges21@gmail.com>
2024-05-13 12:02:37 -0300

    Initial commit

---
+++ cadastro.js
@@ -0,0 +1,18 @@
+let clientes = []
+
+function addClient(){
+    let clientNameInput = document.querySelector('#clientName');
+    let clientName = clientNameInput.value.trim();
+
+    if (clientName === "") { // Condição para se o campo de nome estiver vazio alertar a que tem q ser prenchido
+        alert('Por favor, insera o nome do cliente!');
+    } else if (!/^[A-Z][a-z]*$/.test(clientName)){
+        alert('Por favor, insira um nome válido, tente colocar a primeira letra Maiuscula!');
+    } else if (clientes.includes(clientName)) { // Condição caso o nome do cliente ja esteja na lista
+        alert('Este cliente já está cadastrado!');
+    } else {
+        clientes.push(clientName);
+        console.log(`Clientes: ${clientes}`);
+        clientNameInput.value = ''; // Para limpar o imput quando colocado o nome
+    }
+}
