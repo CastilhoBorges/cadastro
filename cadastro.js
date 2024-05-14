f7d8d4c9d39f044acdcb89fc7c87c3699cdc6251
Augusto Castilho Borges <augustocastilhoborges21@gmail.com>
2024-05-14 12:07:14 -0300

    Sistema de remoção e limpar

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,6 @@
 let clientes = []
 
+// Função para Adicionar o nome na ARRAY
 function addClient(){
     let clientNameInput = document.querySelector('#clientName');
     let clientName = clientNameInput.value.trim();
@@ -14,5 +15,42 @@ function addClient(){
         clientes.push(clientName);
         console.log(`Clientes: ${clientes}`);
         clientNameInput.value = ''; // Para limpar o imput quando colocado o nome
+        
+        if(clientes.length > 0){ // Condição para exibir o remover clientes quando ele estiver na Array
+        document.querySelector('#removeSection').style.display = 'block';
+        }
     }
 }
+
+// Função para remover cliente da ARRAY
+function removeClient(){
+    let removeClientNameInput = document.querySelector('#removeClientName');
+    let clientName = removeClientNameInput.value.trim();
+
+    if(clientName === ""){
+        alert('Por favor, insira o nome do cliente que deseja remover!');
+    } else if(!clientes.includes(clientName)){ // Condição caso o cliente não esteja na ARRAY
+        alert('Este cliente não está cadastrado!');
+    } else{
+        clientes = clientes.filter(cliente => cliente !== clientName); // Comando para remover o cliente
+        console.log(`Clientes: ${clientes}`);
+        removeClientNameInput.value = "";
+
+        if(clientes.length === 0){
+            document.querySelector('#removeSection').style.display = 'none';
+        }
+    }
+}
+
+// Funcões para limpar os inputs quando estiverem com algo escrito
+function clearInput() { 
+    document.getElementById("clientName").value = "";
+}
+function clearRemoveInput() {
+    document.getElementById("removeClientName").value = "";
+}
+
+
+
+
+
